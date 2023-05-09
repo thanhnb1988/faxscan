aa135f9167d470165902dfd266758cd44182746a
thanhnb <thanhnb.hanu@gmail.com>
2023-05-09 16:17:52 +0700

    update bin folder

--- SendFaxApp/Model/FaxDocInfo.cs
+++ SendFaxApp/Model/FaxDocInfo.cs
@@ -8,7 +8,13 @@ namespace SendFaxApp.Model
 {
     public class FaxDocInfo
     {
+        public FaxDocInfo()
+        {
+            Bodies = new List<string>();
+
+        }
         public string DocumentName { get; set; }
         public string Body { get; set; }
+        public List<string> Bodies { get; set; }
     }
 }
